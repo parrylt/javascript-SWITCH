a5ed6732b3effb2c97a5716dc48553b597a9b567
Rômulo Junior <132522416+parrylt@users.noreply.github.com>
2023-09-09 01:20:27 -0300

    Add files via upload

---
+++ js/javinha.js
@@ -0,0 +1,18 @@
+﻿document.write("<h1>Case<h1>");
+var op = parseInt(prompt("Digite um número"));
+switch(op)
+{
+	case 1: 
+	document.write("+");
+	break;
+	case 2: 
+	document.write("-");
+	break;
+	case 3: 
+	document.write("*");
+	break;
+	default: 
+	document.write("Opção inválida");
+	break;
+	
+}
